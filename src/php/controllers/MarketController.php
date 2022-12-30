1b834537f25aa2ac31bdf19a74f6e2edac32f906
torres-developer <torres.dev@disroot.org>
2022-12-30 01:41:08 +0000

    Default controller basic

--- src/php/controllers/MarketController.php
+++ src/php/controllers/MarketController.php
@@ -1,15 +1,56 @@
 <?php
 
 /**
+ *    Blocks Engine - Template for a market place.
+ *    Copyright (C) 2022 João Torres
  *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package TorresDeveloper\\BlocksEngine\\Controllers
+ * @author João Torres <torres.dev@disroot.org>
+ * @copyright Copyright (C) 2022 João Torres
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License
+ * @license https://opensource.org/licenses/AGPL-3.0 GNU Affero General Public License version 3
+ *
+ * @since 0.0.3
+ * @version 0.0.1
  */
 
-use TorresDeveloper\MVC\Controller;
+declare(strict_types=1);
 
+namespace TorresDeveloper\BlocksEngine\Controllers;
+
+use TorresDeveloper\MVC\Controller;
+use TorresDeveloper\MVC\Route;
+
+/**
+ * The default Controller.
+ *
+ * @author João Torres <torres.dev@disroot.org>
+ *
+ * @since 0.0.3
+ * @version 0.0.1
+ */
 class MarketController extends Controller
 {
+    #[Route]
     public function index(): void
     {
-        var_dump($this->req->getUri());
+        include "index.html";
+
+        //echo "<pre>". (string) $this->req->getUri() . PHP_EOL;
+        //var_dump($this->req->getUri());
+        //echo PHP_EOL . "</pre>";
     }
 }

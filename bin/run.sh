fce126b8d08273193749a4a523e460b548022a29
torres-developer <torres.dev@disroot.org>
2023-01-05 21:55:35 +0000

    starts go server auto too

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,10 @@ port=8000
 ip=$(ip -o -4 a | awk '/^[0-9]+: (?:e|w)/' | sed 1q | awk '{print $4}' | cut -d/ -f1)
 folder=$(git rev-parse --show-toplevel)
 
+composer u
+
+cd "$folder"/src/server || exit
+go mod tidy
+go run server.go &
+cd "$folder" || exit
 php -S "$ip":"$port" -t "$folder"/public

8d03e40cc29d05d75620c03b9dde0380f58a1be1
torres-developer <torres.dev@disroot.org>
2023-01-22 02:47:30 +0000

    Models

---
+++ src/models/Client.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ *    Blocks Engine - Template for a market place.
+ *    Copyright (C) 2022-2023 João Torres
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package TorresDeveloper\\BlocksEngine\\Models
+ * @author João Torres <torres.dev@disroot.org>
+ * @copyright Copyright (C) 2022-2023 João Torres
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License
+ * @license https://opensource.org/licenses/AGPL-3.0 GNU Affero General Public License version 3
+ *
+ * @since 0.0.3
+ * @version 0.0.1
+ */
+
+declare(strict_types=1);
+
+namespace TorresDeveloper\BlocksEngine\Models;
+
+use TorresDeveloper\MVC\Model\Model;
+
+/**
+ * Client Model
+ *
+ * @author João Torres <torres.dev@disroot.org>
+ *
+ * @since 0.0.3
+ * @version 0.0.2
+ */
+class Client extends Model
+{
+    public function toArray(): array
+    {
+        return (array) $this;
+    }
+
+    public function toJSON(): string
+    {
+        return json_encode($this);
+    }
+
+    public function toCVS(): string
+    {
+        return (string) $this;
+    }
+
+    public function toTVS(): string
+    {
+        return (string) $this;
+    }
+
+    public function toXML(): string
+    {
+        return (string) $this;
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this;
+    }
+}
